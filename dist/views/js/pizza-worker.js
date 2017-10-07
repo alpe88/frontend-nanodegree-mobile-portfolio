8644db99b527e2941d7305d9743224c8d6df9179
aleksandar <alpe88.dev@gmail.com>
2017-10-07 08:51:29 -0700

    mirrored dist to src

--- dist/views/js/pizza-worker.js
+++ dist/views/js/pizza-worker.js
@@ -1,4 +1,24 @@
-/*! frontend-nanodegree-mobile-portfolio 04-10-2017 */
+//Pizza making code
+importScripts("pizza-generator.js");
 
-importScripts("pizza-generator.js"),onmessage=function(e){var dataFromMain=e.data;console.log("Data received from main: ",dataFromMain);var result={status:"",allThesePizzas:"not set"};"START"===dataFromMain.status?result={status:"STARTED"}:"STOP"===dataFromMain.status?result={status:"STOPPED"}:"PIZZA-NAME"===dataFromMain.status?(result.status="PIZZA-NAMED",result.allThesePizzas=makeAllPizzasAsString(),console.log("This is all the pizza: \n",result.allThesePizzas)):result={status:"NOTHING"},postMessage(result)};
-//# sourceMappingURL=pizza-worker.js.map
+onmessage = function(e) {
+	var dataFromMain = e.data;
+	console.log('Data received from main: ', dataFromMain);
+	var result = {
+		status: "",
+		allThesePizzas: "not set"
+	};
+	var pizzaReady, rngNamedPizza = "";
+	if (dataFromMain.status === "START"){
+		result = {status: "STARTED"};
+	}else if (dataFromMain.status === "STOP"){
+		result = {status: "STOPPED"};
+	}else if (dataFromMain.status === "PIZZA-NAME"){
+		result.status = "PIZZA-NAMED";
+		result.allThesePizzas = makeAllPizzasAsString();
+		console.log('This is all the pizza: \n',result.allThesePizzas);
+	}else{
+		result = {status: "NOTHING"};
+	}
+	postMessage(result);
+};
